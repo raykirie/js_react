fc57fb4d51f3ad0d180f644c616c36e70ac9386b
raykire <sasha.sharifov.2000@mail.ru>
2023-12-16 22:58:17 +0300

    commit

--- frontend/src/Page/BasketPage.js
+++ frontend/src/Page/BasketPage.js
@@ -1,6 +1,7 @@
 import { ReactComponent as XxX } from "../image/x_basket.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { decrAction, incrAction } from "../store/clikerReducer";
+import { removeFromBasketAction } from "../store/basketReducer";
 
 function Basket() {
   const { items } = useSelector((store) => store.basket);
@@ -20,17 +21,33 @@ function Basket() {
             <div className="shopping_cart_info">
               <div className="shopping_cart_up">
             <p className="shopping_cart_title">{item.title}</p>
-            <div className="shopping_cart_photo">
+            <div onClick={() => dispatch(removeFromBasketAction(item))} className="shopping_cart_photo">
               <XxX/>
             </div>
             </div>
+            <div className="shopping_cart_num">
             <div className="shopping_cart_btn">
             <button  onClick={() => dispatch(decrAction(1))}>-</button>
             <p className="shopping_cart_count">{count}</p>
             <button onClick={() => dispatch(incrAction(1))}>+</button>
             </div>
-            <p>${item.price}</p>
-            <p>${item.discont_price}</p>
+            <div className="shopping_cart_price" style={{ display: "flex", alignItems: "baseline" }}>
+                {item.discont_price ? (
+                  <>
+                    <h3 style={{ color: 'var(--txt-black, #282828)', fontSize: '40px', fontWeight: 600, lineHeight: '110%' }}>
+                      ${item.discont_price}
+                    </h3>
+                    <h3 style={{ color: 'var(--txt-grey, #8B8B8B)', fontSize: '20px', fontWeight: 500, lineHeight: '130%', textDecorationLine: 'line-through', marginLeft: '10px' }}>
+                      ${item.price}
+                    </h3>
+                  </>
+                ) : (
+                  <h3 style={{ color: 'var(--txt-black, #282828)', fontSize: '40px', fontWeight: 600, lineHeight: '110%' }}>
+                    ${item.price}
+                  </h3>
+                )}
+              </div> 
+            </div>
             </div>
           </div>
         ))}
--- frontend/src/store/basketReducer.js
+++ frontend/src/store/basketReducer.js
@@ -5,18 +5,19 @@ const defaultState = {
   
 
   const ADD_TO_BASKET = 'ADD_TO_BASKET';
+  const REMOVE_FROM_BASKET = 'REMOVE_FROM_BASKET';
   
   export const basketReducer = (state = defaultState, action) => {
     switch (action.type) {
       case ADD_TO_BASKET:
-        return {
-          ...state,
-          items: [...state.items, action.payload],
-        };
+        return { ...state, items: [...state.items, action.payload],};
+        case REMOVE_FROM_BASKET:
+      return { ...state,items: state.items.filter(item => item.id !== action.payload.id), };
       default:
         return state;
     }
   };
   
   export const addToBasketAction = (payload) => ({ type: ADD_TO_BASKET, payload });
+  export const removeFromBasketAction = (payload) => ({ type: REMOVE_FROM_BASKET, payload });
   
