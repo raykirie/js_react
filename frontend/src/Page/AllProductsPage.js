2029be7bf8a44f8bf66fc66a95b55dcfd3aa58b7
raykirie <145163524+raykirie@users.noreply.github.com>
2023-12-19 17:47:35 +0300

    commit

--- frontend/src/Page/AllProductsPage.js
+++ frontend/src/Page/AllProductsPage.js
@@ -2,43 +2,60 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "../UI/Button/Button";
 import { fetchAllProducts } from "../asyncAction/productsList";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { addToBasketAction } from "../store/basketReducer";
+import { filterBySaleAction } from "../store/productListReducer";
 
 function AllProductsPage() {
-  const { productList, filter } = useSelector((store) => store.productList);
+  const productList = useSelector((store) => store.productList.productList);
   const dispatch = useDispatch();
   let navigate = useNavigate();
+
+  console.log(productList);
 
   useEffect(() => {
     dispatch(fetchAllProducts());
   }, []);
 
   const handleButtonClick = (event, product) => {
-    event.preventDefault(); 
-    event.stopPropagation(); 
+    event.preventDefault();
+    event.stopPropagation();
 
     const { id, title, discont_price, price, image } = product;
-    dispatch(addToBasketAction({
-      id,
-      title,
-      price,
-      discont_price,
-      image,
-    }));
-    
+    dispatch(
+      addToBasketAction({
+        id,
+        title,
+        price,
+        discont_price,
+        image,
+      })
+    );
   };
 
   const handleNavLinkClick = (event, id) => {
     console.log(`NavLink clicked for product with id ${id}`);
   };
 
-  
+  function checkBoxHandler(e) {
+    console.log(e.target.checked);
+    dispatch(filterBySaleAction(e.target.checked));
+  }
+
+  const filteredProductList = productList.filter((elem) => elem.isShow);
+
+
   return (
     <div className="products_all">
       <p>All products</p>
+      <div className="filter_products">
+        <label>
+          Sale
+          <input onClick={checkBoxHandler} type="checkbox" />
+        </label>
+      </div>
       <div className="products-container_all">
-        {productList.map((elem) => (
+        {filteredProductList.map((elem) => (
           <Link
             key={elem.id}
             to={`/products/${elem.id}`}
@@ -46,33 +63,63 @@ function AllProductsPage() {
           >
             <div key={elem.id} className="products-item_all">
               <div className="image-container">
-                <img width={279} height={280} src={"http://localhost:3333/" + elem.image} alt={elem.title}/>
+                <img
+                  width={279}
+                  height={280}
+                  src={"http://localhost:3333/" + elem.image}
+                  alt={elem.title}
+                />
                 <div className="overlay">
-                  <Button onClick={(event) => handleButtonClick(event, elem)} theme="green" title="Add to card" />
+                  <Button
+                    onClick={(event) => handleButtonClick(event, elem)}
+                    theme="green"
+                    title="Add to card"
+                  />
                 </div>
                 <div className="product-title">
-                <p className="sales-card-title">
-                  {elem.title}
-                </p>
+                  <p className="sales-card-title">{elem.title}</p>
                 </div>
-              <div style={{ display: "flex", alignItems: "baseline" }}>
-                {elem.discont_price ? (
-                  <>
-                    <h3 style={{ color: 'var(--txt-black, #282828)', fontSize: '40px', fontWeight: 600, lineHeight: '110%' }}>
-                      ${elem.discont_price}
-                    </h3>
-                    <h3 style={{ color: 'var(--txt-grey, #8B8B8B)', fontSize: '20px', fontWeight: 500, lineHeight: '130%', textDecorationLine: 'line-through', marginLeft: '10px' }}>
+                <div style={{ display: "flex", alignItems: "baseline" }}>
+                  {elem.discont_price ? (
+                    <>
+                      <h3
+                        style={{
+                          color: "var(--txt-black, #282828)",
+                          fontSize: "40px",
+                          fontWeight: 600,
+                          lineHeight: "110%",
+                        }}
+                      >
+                        ${elem.discont_price}
+                      </h3>
+                      <h3
+                        style={{
+                          color: "var(--txt-grey, #8B8B8B)",
+                          fontSize: "20px",
+                          fontWeight: 500,
+                          lineHeight: "130%",
+                          textDecorationLine: "line-through",
+                          marginLeft: "10px",
+                        }}
+                      >
+                        ${elem.price}
+                      </h3>
+                    </>
+                  ) : (
+                    <h3
+                      style={{
+                        color: "var(--txt-black, #282828)",
+                        fontSize: "40px",
+                        fontWeight: 600,
+                        lineHeight: "110%",
+                      }}
+                    >
                       ${elem.price}
                     </h3>
-                  </>
-                ) : (
-                  <h3 style={{ color: 'var(--txt-black, #282828)', fontSize: '40px', fontWeight: 600, lineHeight: '110%' }}>
-                    ${elem.price}
-                  </h3>
-                )}
-              </div> 
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
           </Link>
         ))}
       </div>
--- frontend/src/store/productListReducer.js
+++ frontend/src/store/productListReducer.js
@@ -7,18 +7,30 @@ const defaultState = {
 const ALL_PRODUCTS = 'ALL_PRODUCTS'
 const SALES_PRODUCTS = 'SALES_PRODUCTS'
 const PRODUCTS_BY_CATEGORY = 'PRODUCTS_BY_CATEGORY'
-
+const FILTER_BY_SALE = 'FILTER_BY_SALE'
 
 
 
 export const productListReducer = (state = defaultState, action) => {
     switch(action.type){
         case ALL_PRODUCTS:
-            return {productList: action.payload}
+            return {productList: action.payload.map(elem => {
+                elem.isShow = true
+                return elem
+            })}
         case SALES_PRODUCTS:
             return {productList: action.payload}
         case PRODUCTS_BY_CATEGORY:
             return {productList: action.payload.data}
+        case FILTER_BY_SALE:
+            return {productList: state.productList.map(elem => {
+                if (action.payload){
+                    elem.isShow = (elem.discont_price) ? true : false
+                } else {
+                    elem.isShow = true
+                }
+                return elem
+            })}
         default:
             return state
     }
@@ -27,5 +39,5 @@ export const productListReducer = (state = defaultState, action) => {
 export const allProductsAction = (payload) => ({type: ALL_PRODUCTS, payload })
 export const salesProductsAction = (payload) => ({type: SALES_PRODUCTS, payload })
 export const productsByCategoryAction = (payload) => ({type: PRODUCTS_BY_CATEGORY, payload })
-
+export const filterBySaleAction = (payload) => ({type: FILTER_BY_SALE, payload})
 
